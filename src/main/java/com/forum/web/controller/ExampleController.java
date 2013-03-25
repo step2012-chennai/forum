aaa65a34392748282345f27d8bf8e035645b8fbc
vikytech <famous.vikhyath@gmail.com>
2013-03-25 17:31:33 +0530

    Vikhyath|Suganthi refactor in User Login(modified the database).

--- src/main/java/com/forum/web/controller/ExampleController.java
+++ src/main/java/com/forum/web/controller/ExampleController.java
@@ -2,6 +2,8 @@ package com.forum.web.controller;
 
 import com.forum.services.ExampleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
@@ -29,24 +31,43 @@ public class ExampleController {
         return mv;
     }
 
+    @RequestMapping(value = "/display", method = RequestMethod.GET)
+    public ModelAndView display() {
+
+        ModelAndView mv = new ModelAndView("home");
+        mv.addObject("thing", null);
+        return mv;
+    }
+
+    @RequestMapping(value = "/postQuestion", method = RequestMethod.GET)
+    public ModelAndView postQuestion() {
+
+        ModelAndView mv = new ModelAndView("postQuestion");
+        mv.addObject("thing", null);
+
+        return mv;
+    }
+
     @RequestMapping(value = "/validate", method = RequestMethod.POST)
     public ModelAndView getInput(@RequestParam("j_username") String username, @RequestParam("j_password") String password) {
         ModelAndView mv;
+        ApplicationContext context = new ClassPathXmlApplicationContext("file:src/main/webapp/WEB-INF/config.xml");
+        exampleService = (ExampleService) context.getBean("login");
 
+        System.out.println(exampleService==null);
         if (exampleService.match(username, password)) {
             mv = new ModelAndView(new RedirectView("activityWall"));
         } else {
             mv = new ModelAndView(new RedirectView("login"));
             mv.addObject("error", "Password/Username is incorrect");
         }
+        return mv;
+    }
+    @RequestMapping(value = "/activityWall", method = RequestMethod.GET)
+    public ModelAndView activityWall() {
+        ModelAndView mv = new ModelAndView("activityWall");
+        mv.addObject("thing", null);
 
         return mv;
     }
-
-    @RequestMapping(value = "/process", method = RequestMethod.POST)
-    public String process(@PathVariable("entityId") String entityId,
-                          @ModelAttribute("conversation") ExampleConversation conversation) {
-        return "viewName";
-    }
-
 }
--- src/unit-tests/java/ExampleServiceTest.java
+++ src/unit-tests/java/ExampleServiceTest.java
@@ -1,33 +1,35 @@
 import com.forum.services.ExampleService;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class ExampleServiceTest {
     ExampleService service;
+    ApplicationContext context;
 
     @Before
-    public void setup(){
+    public void setup() {
         service = new ExampleService();
+        context = new ClassPathXmlApplicationContext("file:src/main/webapp/WEB-INF/config.xml");
+        service = (ExampleService) context.getBean("login");
     }
 
     @Test
-    public void shouldMatchTheUserNameAndPassWord(){
-        ExampleService service = new ExampleService();
+    public void shouldMatchTheUserNameAndPassWord() {
         assertTrue(service.match("user", "password"));
     }
 
     @Test
-    public void shouldNotMatchTheUserNameAndPassWord(){
-        ExampleService service = new ExampleService();
+    public void shouldNotMatchTheUserNameAndPassWord() {
         assertFalse(service.match("usersdfd", "password"));
     }
 
     @Test
-    public void shouldNotMatchSameUsernameAndDifferentPassword(){
-        ExampleService service = new ExampleService();
+    public void shouldNotMatchSameUsernameAndDifferentPassword() {
         assertFalse(service.match("user", "passwordfsf"));
     }
 }
