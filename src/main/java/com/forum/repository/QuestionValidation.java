48eb3de947637a234383e8f1f2b8e80c8c8ef495
ajit <saroya.ajit@yahoo.com>
2013-03-27 16:46:14 +0530

    Ravi,Ajit|added method for inserting Apostrophe Character in Datatbase

--- src/main/java/com/forum/repository/QuestionValidation.java
+++ src/main/java/com/forum/repository/QuestionValidation.java
@@ -12,23 +12,45 @@ public class QuestionValidation {
 
     public boolean isQuestionValid() {
         if (question == null) return false;
-        if (question == "" || question.length() < MINIMUM_CHARACTERS) return false;
-        question = getPlainText(question);
-        int spaces = 0, actualSpaces = 0;
 
-        for (int i = 0; i < question.length(); i++) {
-            if (question.charAt(i) == ' ') {
-                actualSpaces++;
-                continue;
-            }
-            int ch = question.charAt(i);
-            if (ch == 160) spaces++;
-        }
-        return ((question.length() - actualSpaces) != spaces);
+        question = getPlainText(question);
+        question = reduceBlanks(question);
+        if (question == "" || question.length() < MINIMUM_CHARACTERS) return false;
+        return true;
     }
 
     private String getPlainText(String question) {
         question = question.replaceAll("\\<.*?>", "");
         return StringEscapeUtils.unescapeHtml(question);
+    }
+
+    private String reduceBlanks(String question) {
+        int spaceCount = 0;
+        StringBuilder refactoredQuestion = new StringBuilder(question.length());
+
+        for (int i = 0; i < question.length(); i++) {
+            boolean spaceCharacter = question.charAt(i) == 32 || question.charAt(i) == 160;
+            if(spaceCharacter)spaceCount++;
+
+            if (!spaceCharacter || spaceCount <= 1) {
+                refactoredQuestion.append(question.charAt(i));
+            }
+
+            if(!spaceCharacter)spaceCount = 0;
+        }
+
+        return refactoredQuestion.toString();
+    }
+
+    public String insertApostrophe() {
+        StringBuilder refactoredQuestion = new StringBuilder(question.length());
+        String apostrophe = "'";
+        for (int i = 0; i < question.length(); i++ ){
+            refactoredQuestion.append(question.charAt(i));
+            if(question.charAt(i) == '\''){
+                refactoredQuestion.append(apostrophe);
+            }
+        }
+        return refactoredQuestion.toString();
     }
 }
