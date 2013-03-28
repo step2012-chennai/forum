455a2fbb2dda8103b45089da47a8559879283aad
Anil kumar <aniltpl08@gmail.com>
2013-03-28 16:49:09 +0530

    Ravi,Anil|Method added for retriving question based on questions Per page

--- src/main/java/com/forum/repository/ShowQuestions.java
+++ src/main/java/com/forum/repository/ShowQuestions.java
@@ -4,7 +4,9 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
 
 import javax.sql.DataSource;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class ShowQuestions {
     private JdbcTemplate jdbcTemplate;
@@ -18,17 +20,15 @@ public class ShowQuestions {
         this.dataSource = dataSource;
     }
 
-    public SqlRowSet show(int pageNumber,int resultPerPage) {
-        SqlRowSet sqlRowSet;
-        int lastQuestionId=pageNumber*resultPerPage;
-        int numberOfRows = numberOfRows();
-        int firstQuestionId= (numberOfRows-lastQuestionId);
-        sqlRowSet = jdbcTemplate.queryForRowSet("select question from questions where q_id between "+
-               (firstQuestionId)   +" and "+(numberOfRows)+" ORDER BY q_id DESC ");
-        return sqlRowSet;
-    }
-
-    private int numberOfRows() {
-        return jdbcTemplate.queryForInt("select count(*) from questions");
+    public List<String> show(int pageNumber, int questionsPerPage) {
+        int endIndex = pageNumber * questionsPerPage;
+        List<String> questions = jdbcTemplate.queryForList("select question from questions ORDER BY q_id DESC", String.class);
+        List<String> resultQuestions = new ArrayList<String>(questionsPerPage);
+        for (int startIndex = (pageNumber - 1) * questionsPerPage; startIndex < endIndex; startIndex++) {
+            if (startIndex < questions.size()) {
+                resultQuestions.add(questions.get(startIndex));
+            }
+        }
+        return resultQuestions;
     }
 }
