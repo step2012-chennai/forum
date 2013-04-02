b9db67beb604f3f1c344f690b2d962f09b1df021
Anil kumar <aniltpl08@gmail.com>
2013-04-02 18:12:32 +0530

    Pavithra|Anil |failure scenario for postQuestion controller

--- tests/unit-tests/java/com/forum/controller/PostQuestionControllerTest.java
+++ tests/unit-tests/java/com/forum/controller/PostQuestionControllerTest.java
@@ -3,6 +3,7 @@ package com.forum.controller;
 import com.forum.repository.PostQuestion;
 import com.forum.repository.QuestionValidation;
 import org.hamcrest.core.IsEqual;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -19,9 +20,7 @@ import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandl
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 import static org.junit.Assert.assertThat;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({
@@ -32,20 +31,23 @@ public class PostQuestionControllerTest {
     private MockHttpServletRequest mockHttpServletRequest;
     private MockHttpServletResponse mockHttpServletResponse;
     private AnnotationMethodHandlerAdapter handlerAdapter;
+    private PostQuestionController postQuestionController;
+    private String question;
 
-
-    @Test
-    public void shouldInsertGivenValidQuestion() throws Exception {
-        mockHttpServletRequest = new MockHttpServletRequest();
-        mockHttpServletResponse = new MockHttpServletResponse();
-        handlerAdapter = new AnnotationMethodHandlerAdapter();
-        PostQuestionController postQuestionController = new PostQuestionController();
-        String question = "kb";
-
+    @Before
+     public void setUp(){
+         mockHttpServletRequest = new MockHttpServletRequest();
+         mockHttpServletResponse = new MockHttpServletResponse();
+         handlerAdapter = new AnnotationMethodHandlerAdapter();
+        postQuestionController = new PostQuestionController();
+        question = "kb";
         mockHttpServletRequest.setRequestURI("/postedQuestion");
         mockHttpServletRequest.setMethod("POST");
         mockHttpServletRequest.setParameter("textareas", question);
+     }
 
+    @Test
+    public void shouldInsertGivenValidQuestion() throws Exception {
         QuestionValidation mockQuestionValidation = Mockito.mock(QuestionValidation.class);
         when(mockQuestionValidation.isQuestionValid(question)).thenReturn(true);
         ReflectionTestUtils.setField(postQuestionController, "questionValidation", mockQuestionValidation);
@@ -53,7 +55,6 @@ public class PostQuestionControllerTest {
         PostQuestion mockPostQuestion = Mockito.mock(PostQuestion.class);
         doNothing().when(mockPostQuestion).insert(question);
         ReflectionTestUtils.setField(postQuestionController, "post", mockPostQuestion);
-
 
         ModelAndView modelAndView = handlerAdapter.handle(mockHttpServletRequest, mockHttpServletResponse, postQuestionController);
 
@@ -63,8 +64,20 @@ public class PostQuestionControllerTest {
     }
 
     @Test
-    public void shouldAddErrorWhenValidationFails() {
+    public void shouldAddErrorWhenValidationFails() throws Exception {
+        QuestionValidation mockQuestionValidation = Mockito.mock(QuestionValidation.class);
+        when(mockQuestionValidation.isQuestionValid(question)).thenReturn(false);
+        ReflectionTestUtils.setField(postQuestionController, "questionValidation", mockQuestionValidation);
 
+        PostQuestion mockPostQuestion = Mockito.mock(PostQuestion.class);
+        ReflectionTestUtils.setField(postQuestionController, "post", mockPostQuestion);
+
+        ModelAndView modelAndView = handlerAdapter.handle(mockHttpServletRequest, mockHttpServletResponse, postQuestionController);
+
+        verify(mockQuestionValidation).isQuestionValid(question);
+        verify(mockPostQuestion,never()).insert(question);
+        assertThat(modelAndView.getViewName(), IsEqual.equalTo("postQuestion"));
+        assertThat(modelAndView.getModel().get("error").toString(), IsEqual.equalTo("Question length must be of at least 20 characters, and should not contain all spaces"));
     }
 
 }
