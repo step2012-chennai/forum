d7a4e66f26e5f4bed1b0bce9210e4e444c735975
sandeepsinha <sinha.sandeep01@gmail.com>
2013-04-12 22:30:46 +0530

    Sandeep,Prasath | Test written for user validation

--- tests/unit-tests/java/com/forum/controller/RegistrationControllerTest.java
+++ tests/unit-tests/java/com/forum/controller/RegistrationControllerTest.java
@@ -3,12 +3,14 @@ package com.forum.controller;
 import com.forum.repository.UserRepository;
 import org.hamcrest.core.IsEqual;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.ModelAndView;
 
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.when;
 
 public class RegistrationControllerTest extends BaseController{
+    @Autowired
     UserRepository userRepository;
     RegistrationController registrationController;
     @Test
@@ -22,24 +24,24 @@ public class RegistrationControllerTest extends BaseController{
     @Test
     public void shouldValidateTheUserNameAlreadyExist() throws Exception {
         registrationController=new RegistrationController();
-        mockHttpServletRequest.setRequestURI("/validate");
+        mockHttpServletRequest.setRequestURI("/validateUserName");
         mockHttpServletRequest.setParameter("user", "user");
         mockHttpServletRequest.setMethod("GET");
         userRepository = (UserRepository) createMock(registrationController, "userRepository", UserRepository.class);
         when(userRepository.isUserNameExists("user")).thenReturn(true);
-        ModelAndView modelAndView=handlerAdapter.handle(mockHttpServletRequest,mockHttpServletResponse,registrationController);
-        assertThat(modelAndView.getViewName(), IsEqual.equalTo("Already available"));
+        assertThat(registrationController.ValidateUserName("user"), IsEqual.equalTo("Already available"));
     }
 
     @Test
     public void shouldReturnCorrectIfUserNameNotExist() throws Exception {
         registrationController=new RegistrationController();
-        mockHttpServletRequest.setRequestURI("/validate");
+        mockHttpServletRequest.setRequestURI("/validateUserName");
         mockHttpServletRequest.setParameter("user", "wrongUser");
         mockHttpServletRequest.setMethod("GET");
         userRepository = (UserRepository) createMock(registrationController, "userRepository", UserRepository.class);
         when(userRepository.isUserNameExists("user")).thenReturn(true);
-        ModelAndView modelAndView=handlerAdapter.handle(mockHttpServletRequest,mockHttpServletResponse,registrationController);
-        assertThat(modelAndView.getViewName(), IsEqual.equalTo("correct"));
+        when(userRepository.isUserNameExists("user")).thenReturn(true);
+        assertThat(registrationController.ValidateUserName("user"), IsEqual.equalTo("Already available"));
+
     }
 }
