a450b30736748dc07448570a2bddb53fe816bc0d
Prasanth <prasantd@thoughtworks.com>
2013-04-14 11:15:51 +0530

    Ajit, Prasath|(terms and condition) print functionality and terms and conditions is added

---
+++ src/main/webapp/static/javascript/printDiv.js
@@ -0,0 +1,16 @@
+ function PrintElem(elem){
+         Popup($(elem).html());
+ }
+
+ function Popup(data){
+     var mywindow = window.open('', 'my div', 'height=400,width=600');
+     mywindow.document.write('<html><head><title>my div</title>');
+     mywindow.document.write('</head><body >');
+     mywindow.document.write(data);
+     mywindow.document.write('</body></html>');
+
+     mywindow.print();
+     mywindow.close();
+
+     return true;
+ }
